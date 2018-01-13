c57a518f0da3422ec19eb1942e47cd88404860fe
Lisa Lee <snickers495@gmail.com>
2018-01-13 17:25:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(arr) {
+  return arr.map(word => word.toLowerCase())
+}
+
+function nameToAttributes(arr) {
+  const newArr = arr.map(function(word) {
+    const newWord = word.split(" ");
+    const first = {firstName: newWord[0]};
+    const last = {lastName: newWord[1]};
+    return Object.assign(first, last);
+  });
+  return newArr;
+}
+function attributesToPhrase(arr) {
+  return arr.map(function(word) {
+    return word.name + " is from " + word.hometown;
+  });
+}
