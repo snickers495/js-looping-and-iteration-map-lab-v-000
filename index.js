c05bb9ff9988de166a5451fedd16c33f6d415f3f
Lisa Lee <snickers495@gmail.com>
2018-01-13 17:21:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,3 @@ function lowerCaseDrivers(arr) {
   return arr.map(word => word.toLowerCase())
 }
 
-function nameToAttributes(arr) {
-  const newArr = arr.map(function(word) {
-    newWord = word.split(" ");
-    Object.assign(firstName: newWord[0], lastName: newWord[1]);
-  })
-  return newArr;
-}
