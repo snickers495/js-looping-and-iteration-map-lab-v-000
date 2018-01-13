a70adb22ea3a1143e6ff78e8c6ad92c3d92cbf50
Lisa Lee <snickers495@gmail.com>
2018-01-13 17:23:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function nameToAttributes(arr) {
   const newArr = arr.map(function(word) {
     const newWord = word.split(" ");
     const first = {firstName: newWord[0]};
-    const last = {lastName: newWord[1]}
-    return Object.assign((firstName: newWord[0]), (lastName: newWord[1]));
+    const last = {lastName: newWord[1]};
+    return Object.assign(first, last);
   })
   return newArr;
 }
