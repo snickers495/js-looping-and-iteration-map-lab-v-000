921f82df22c8109830a69327a82ff8143ab7ea7e
Lisa Lee <snickers495@gmail.com>
2018-01-13 17:20:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function lowerCaseDrivers(arr) {
 function nameToAttributes(arr) {
   newArr = arr.map(function(word) {
     newWord = word.split(" ");
-    return Object.assign(firstName: newWord[0], lastName: newWord[1])
+    return Object.assign(firstName: newWord[0], lastName: newWord[1]);
   })
   return newArr;
 }
