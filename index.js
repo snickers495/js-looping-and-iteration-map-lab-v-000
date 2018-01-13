5aca24a086e160c1e417274248a584dfbc41a9f4
Lisa Lee <snickers495@gmail.com>
2018-01-13 17:23:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function lowerCaseDrivers(arr) {
 function nameToAttributes(arr) {
   const newArr = arr.map(function(word) {
     const newWord = word.split(" ");
+    const first = {firstName: newWord[0]};
+    const last = {lastName: newWord[1]}
     return Object.assign((firstName: newWord[0]), (lastName: newWord[1]));
   })
   return newArr;
